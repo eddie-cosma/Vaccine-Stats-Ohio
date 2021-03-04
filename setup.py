a9d1faa5cf2072e66b1cddfc9f3ca75b2e96b0f7
Eddie Cosma <36821604+eddie-cosma@users.noreply.github.com>
2021-03-04 17:06:40 -0500

    increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="vaccine-stats-ohio",
-    version="1.0r10",
+    version="1.0r11",
     description="Access and summarize Ohio COVID vaccine statistics",
     author="Eddie Cosma",
     author_email="vaxstat@eddiecosma.com",
