b083e1867c8e38b3613ee51366a4b1b73c183ca3
Eddie Cosma <eddie@pydev.eddiecosma.com>
2021-03-07 22:15:44 -0500

    changed tests to use static data

--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,30 @@
 import unittest
 from vaccine_stats import Vax_Stats
 from datetime import date
+import pathlib
+
+SELF_PATH = pathlib.Path(__file__).parent.absolute()
+DATA_PATH = SELF_PATH.joinpath("test_stats.csv").as_uri()
+Vax_Stats.ODH_URL = DATA_PATH
 
 
 class TestStats(unittest.TestCase):
     def test_lookup(self):
         with Vax_Stats() as stats:
             p, q = stats.lookup("All", date(2021, 1, 1))
-        self.assertEqual(p, 176955)
-        self.assertEqual(q, 50)
+        self.assertEqual(p, 177063)
+        self.assertEqual(q, 60)
 
     def test_lookup_noncumulative(self):
         with Vax_Stats() as stats:
             p, q = stats.lookup("All", date(2021, 1, 1), False)
-        self.assertEqual(p, 504)
-        self.assertEqual(q, 19)
+        self.assertEqual(p, 527)
+        self.assertEqual(q, 20)
 
     def test_lookup_county(self):
         with Vax_Stats() as stats:
             p, q = stats.lookup("Cuyahoga", date(2021, 1, 1))
-        self.assertEqual(p, 20871)
+        self.assertEqual(p, 20883)
         self.assertEqual(q, 2)
 
     def test_lookup_noncumulative_county(self):
@@ -32,29 +37,29 @@ class TestStats(unittest.TestCase):
         with Vax_Stats() as stats:
             p, q = stats.delta("All", date(2021, 1, 1), date(2021, 2, 1))
         self.assertEqual(round(p, 0), 421)
-        self.assertEqual(round(q, 0), 446912)
+        self.assertEqual(round(q, 0), 372975)
 
     def test_delta_absolute(self):
         with Vax_Stats() as stats:
             p, q = stats.delta(
                 "All", date(2021, 1, 1), date(2021, 2, 1), False
             )
-        self.assertEqual(p, 745057)
-        self.assertEqual(q, 223456)
+        self.assertEqual(p, 745839)
+        self.assertEqual(q, 223785)
 
     def test_delta_percent_county(self):
         with Vax_Stats() as stats:
             p, q = stats.delta("Cuyahoga", date(2021, 1, 1), date(2021, 2, 1))
         self.assertEqual(round(p, 0), 356)
-        self.assertEqual(round(q, 0), 1255650)
+        self.assertEqual(round(q, 0), 1257500)
 
     def test_delta_absolute_county(self):
         with Vax_Stats() as stats:
             p, q = stats.delta(
                 "Cuyahoga", date(2021, 1, 1), date(2021, 2, 1), False
             )
-        self.assertEqual(p, 74204)
-        self.assertEqual(q, 25113)
+        self.assertEqual(p, 74286)
+        self.assertEqual(q, 25150)
 
     def test_percent_vaccinated_full(self):
         with Vax_Stats() as stats:
